7a87f0b4ba5123e1df1ae04023149b07e9ce8608
Matteo Bertini <matteo@naufraghi.net>
2015-09-15 00:40:33 +0200

    Resize page by a magic number to fill the paper

--- svg_output_demo.py
+++ svg_output_demo.py
@@ -15,40 +15,13 @@ def main():
 
 
     def paint_svg(printer, painter, page):
-        print("  Rendering page outline")
-        page_rect = printer.pageRect(QPrinter.DevicePixel)
-        """
-        page_pos = QPointF(page_rect.x(), page_rect.y())
-        # fake print, triggers the layout at printer resolution for SvgPlaceholder
-        page.resize(page_rect.width(), page_rect.height())
-        # make qwidget output vectorial providing a non-qprinter device
+        page_rect = printer.pageRect(QPrinter.Point)
+        print("  Rendering page size = %s, resolution = %s" % (page_rect.size(), printer.resolution()))
+        page.resize(page_rect.width()/0.6, page_rect.height()/0.6)
+        print("  physicalDpi = %s %s" % (printer.physicalDpiX(), printer.physicalDpiY()))
+        # make qwidget output vectorial, rendering directly on a painter
+        # results in a raster image in the pdf
         page_pic = QPicture()
-        wpainter = QPainter(page_pic)
-        page.render(wpainter, flags=QWidget.DrawChildren)
-        wpainter.end()
-
-        svg_placeholders = page.findChildren(parser.SvgPlaceholder)
-        print("  Filling %d svg placeholders..." % len(svg_placeholders))
-        for svg_placeholder in svg_placeholders:
-            placeholder_box = QRectF(svg_placeholder.frameGeometry())
-
-            # if the page is at screen resolution
-            #placeholder_box.translate(placeholder_box.x()*9, placeholder_box.y()*9)
-            #placeholder_box.setSize(placeholder_box.size()*9)
-
-            print("    placeholder_box = %s, pos = %s" % (placeholder_box, svg_placeholder.pos()))
-            svg = QSvgRenderer(svg_placeholder.src)
-            print("    svg size = %s box = %s" % (svg.defaultSize(), svg.viewBox()))
-            svg_box = svg.viewBoxF()
-            svg_size = svg_box.size()
-            svg_size.scale(placeholder_box.size(), Qt.KeepAspectRatio)
-            svg_box.setSize(svg_size)
-            svg_box.translate(placeholder_box.center() - svg_box.center())
-            svg.render(painter, svg_box)
-        """
-        # real print
-        page.resize(page_rect.width(), page_rect.height())
-        page_pic = QPicture() # make qwidget output vectorial
         wpainter = QPainter(page_pic)
         page.render(wpainter, flags=QWidget.DrawChildren)
         wpainter.end()
@@ -59,14 +32,14 @@ def main():
         printer = QPrinter(QPrinter.HighResolution)
         printer.setOutputFormat(QPrinter.PdfFormat)
         printer.setOutputFileName("report.pdf")
-        # printer.setFullPage(True)
+        printer.setColorMode(QPrinter.Color)
+        printer.setFullPage(True)
 
         print("Parsing template")
         pages = parser.parse(open("image-preview.wrp"))
 
         print("Composing output pdf")
-        painter = QPainter()
-        painter.begin(printer)
+        painter = QPainter(printer)
         print("Adding %d pages..." % len(pages))
         for i, page in enumerate(pages):
             if i > 0: # dopo la prima
