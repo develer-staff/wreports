77b9d1694f9100980d665e849354c893d48c26cb
Matteo Bertini <matteo@naufraghi.net>
2015-09-15 16:51:37 +0200

    Add setup metadata

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(name='wreports',
       version='0.1.0',
-      author='',
-      description='wreports description',
+      author='Matteo Bertini <naufraghi@develer.com>',
+      description='Fixed page report generator',
       long_description=open('README.rst').read(),
       license='LICENSE.txt',
       keywords="",
@@ -11,7 +11,6 @@ setup(name='wreports',
       # package source directory
       package_dir={'': 'src'},
       packages=find_packages('src', exclude='docs')
-
 
     # configure the default test suite
     , test_suite='tests.suite'
